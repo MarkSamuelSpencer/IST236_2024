129ca411a9756d75ee6c9c20764086e4f360808f
MarkSamuelSpencer <marksamuelspencer1997@gmail.com>
2024-02-19 21:43:48 -0500

    Created Note Taking App and Week 6 Folder

---
+++ Week6/ThoughtVault/screens/HomeScreen.js
@@ -0,0 +1,73 @@
+import { Image, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import NavButton from "../components/NavButton";
+import { useFonts } from "expo-font";
+import Title from "../components/Title";
+import Colors from "../constants/colors";
+
+function HomeScreen(props) {
+  //set safe area screen boundaries
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        styles.rootContainer,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+      ]}
+    >
+      <View style={styles.titleContainer}>
+        <Title>Thought Vault</Title>
+      </View>
+
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/note-taking.jpg")}
+          style={styles.image}
+        />
+      </View>
+
+      <View style={styles.navButtonContainer}>
+        <NavButton onNext={props.onNext}>Go To Notes</NavButton>
+      </View>
+    </View>
+  );
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    width: "90%",
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  imageContainer: {
+    flex: 2,
+    justifyContent: "center",
+    borderWidth: 4,
+    borderRadius: 55,
+    borderColor: Colors.accent500,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 50,
+    resizeMode: "stretch",
+  },
+  navButtonContainer: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
